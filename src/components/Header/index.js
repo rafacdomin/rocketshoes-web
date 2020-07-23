b5ce844aba693eb1500042b956cb8de79efe526f
Rafael <rafaelcodomingues@gmail.com>
2020-07-22 23:29:22 -0300

    README

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -9,23 +9,23 @@ import { Container, Cart } from './styles';
 import logo from '../../assets/images/logo.svg';
 
 function Header({ cartSize }) {
-    return (
-        <Container>
-            <Link to="/">
-                <img src={logo} alt="RocketShoes" />
-            </Link>
+  return (
+    <Container>
+      <Link to="/">
+        <img src={logo} alt="RocketShoes" />
+      </Link>
 
-            <Cart to="/cart">
-                <div>
-                    <strong>Meu Carrinho</strong>
-                    <span>{cartSize} itens</span>
-                </div>
-                <MdShoppingBasket size={36} color="#FFF" />
-            </Cart>
-        </Container>
-    );
+      <Cart to="/cart">
+        <div>
+          <strong>My Cart</strong>
+          <span>{cartSize} itens</span>
+        </div>
+        <MdShoppingBasket size={36} color="#FFF" />
+      </Cart>
+    </Container>
+  );
 }
 
 export default connect((state) => ({
-    cartSize: state.cart.length,
+  cartSize: state.cart.length,
 }))(Header);
--- src/styles/global.js
+++ src/styles/global.js
@@ -15,7 +15,7 @@ export default createGlobalStyle`
     }
 
     body{
-        background: #191920 url(${background}) no-repeat center top;
+        background: #7151c1 url(${background}) no-repeat center top;
         -webkit-font-smoothing: antialiased;
     }
 
