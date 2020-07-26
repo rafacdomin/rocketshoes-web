9ddd0dc609aa5a95069da7af2c752f464d848a54
Rafael <rafaelcodomingues@gmail.com>
2020-07-26 13:30:48 -0300

    Add Hooks

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,32 +1,34 @@
 module.exports = {
-    env: {
-        es2020: true,
+  env: {
+    es2020: true,
+  },
+  extends: ['airbnb', 'prettier', 'prettier/react'],
+  globals: {
+    __DEV__: 'readonly',
+  },
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    extends: ['airbnb', 'prettier', 'prettier/react'],
-    globals: {
-        __DEV__: 'readonly',
-    },
-    parser: 'babel-eslint',
-    parserOptions: {
-        ecmaFeatures: {
-            jsx: true,
-        },
-        ecmaVersion: 11,
-        sourceType: 'module',
-    },
-    plugins: ['react', 'prettier'],
-    rules: {
-        'prettier/prettier': 'error',
-        'react/jsx-filename-extension': [
-            'warn',
-            {
-                extensions: ['.jsx', '.js'],
-            },
-        ],
-        'import/prefer-default-export': 'off',
-        'no-console': 'off',
-        'react/state-in-constructor': 'off',
-        'react/static-property-placement': 'off',
-        'no-param-reassign': 'off'
-    },
+    ecmaVersion: 11,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'prettier', 'react-hooks'],
+  rules: {
+    'prettier/prettier': 'error',
+    'react/jsx-filename-extension': [
+      'warn',
+      {
+        extensions: ['.jsx', '.js'],
+      },
+    ],
+    'import/prefer-default-export': 'off',
+    'no-console': 'off',
+    'react/state-in-constructor': 'off',
+    'react/static-property-placement': 'off',
+    'no-param-reassign': 'off',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+  },
 };
